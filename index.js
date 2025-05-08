53746d3aefaab6fdb8d040900a0d38bf1eefe7f4
Sepehr Joneidi <sepehrjoneidi7@gmail.com>
2025-05-08 11:07:43 +0200

    My Calculator

---
+++ index.js
@@ -0,0 +1,41 @@
+ let number1 = 0;
+  let number2 = 0;
+  let result=0;
+
+  function getNumbersFromTextbox() {
+   number1=parseInt(document.getElementById('firstnumber').value);
+   number2=parseInt(document.getElementById('secondnumber').value);
+  }
+
+  function emptyBox() {
+    document.getElementById('firstnumber').value='';
+    document.getElementById('secondnumber').value='';
+  }
+  
+  function add(){
+   getNumbersFromTextbox();
+   result=number1+number2
+   document.getElementById('result').innerHTML=result;
+   emptyBox();
+  }
+
+ function minus(){
+  getNumbersFromTextbox();
+  result=number1-number2
+  document.getElementById('result').innerHTML=result;
+  emptyBox();
+ }
+ 
+ function multiple(){
+  getNumbersFromTextbox();
+  result=number1*number2
+  document.getElementById('result').innerHTML=result;
+  emptyBox();
+ }
+ 
+ function devide(){
+  getNumbersFromTextbox();
+  result=number1/number2
+  document.getElementById('result').innerHTML=result; 
+  emptyBox();
+ }
